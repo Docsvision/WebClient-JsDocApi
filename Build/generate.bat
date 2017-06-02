32d2bc05b738ccc3afe8363e1bc1a03e40a1aac7
Elkhov Denis <Elkhov.D@docsvision.com>
2017-06-02 13:31:03 +0300

    move output folder

--- Build/generate.bat
+++ Build/generate.bat
@@ -2,6 +2,6 @@ copy /Y "..\Bin\layout-extension.d.ts" "TypeScriptDefinitions\layout-extension.d
 copy /Y "..\Bin\TypeScriptDefinitions\web-client-legacy.d.ts" "TypeScriptDefinitions\web-client-legacy.d.ts"
 
 :: For TypeDoc installation see http://typedoc.org/guides/installation/#command-line-interface
-typedoc --theme WebClientTypedocTheme  --name "Docsvision 5 Web-client. JavaScript API"  --readme README.md --includeDeclarations --excludePrivate --mode file --entryPoint WebClient --excludeExternals --logger none --ignoreCompilerErrors --out Output/docs TypeScriptDefinitions/
+typedoc --theme WebClientTypedocTheme  --name "Docsvision 5 Web-client. JavaScript API"  --readme README.md --includeDeclarations --excludePrivate --mode file --entryPoint WebClient --excludeExternals --logger none --ignoreCompilerErrors --out ../docs TypeScriptDefinitions/
 
 
