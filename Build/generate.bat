728ebd09d2b3917eeef35b84aa131f7d380f1efb
Bogatyrev.P <Bogatyrev.P@docsvision.com>
2018-11-15 11:34:27 +0300

    Updated 5.4.11 to 5.5.11.

--- Build/generate.bat
+++ Build/generate.bat
@@ -1,5 +1,5 @@
 :: For TypeDoc installation see http://typedoc.org/guides/installation/#command-line-interface
-typedoc --theme WebClientTypedocTheme  --name "Docsvision 5 Web-client 5.4.11. JavaScript API"  --readme ..\README.md --includeDeclarations --excludePrivate --mode file --excludeExternals --logger none --ignoreCompilerErrors --out ../docs TypeScriptDefinitions/
+typedoc --theme WebClientTypedocTheme  --name "Docsvision 5 Web-client 5.5.11. JavaScript API"  --readme ..\README.md --includeDeclarations --excludePrivate --mode file --excludeExternals --logger none --ignoreCompilerErrors --out ../docs TypeScriptDefinitions/
 
 xcopy /E /I /Y /R ../docs-webclient9 ../docs/docs-webclient9
 xcopy /E /I /Y /R ../docs-webclient10 ../docs/docs-webclient10
